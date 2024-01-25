fa7595d6af4e8adbc9a4b539673d1d409dcf5c46
Luiz Henrique <luizboaventura24@gmail.com>
2024-01-25 20:35:34 -0300

    4/5 seções criadas

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'text-primary': '#1A1A1A',
+        'text-secondary': '#663BC1',
+        'primary-color': '#E7E7E7',
+        'secondary-color': '#242424',
+        'third-color': '#2d2d2d'
+      },
+    }
+  }
+}
